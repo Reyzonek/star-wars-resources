1da58bd975bc0038ee200fddc62d762168626f93
Pawel Pograniczny <pawel.pograniczny@tsh.io>
2024-12-07 12:54:57 +0100

    feat(SWR-2): create swapi client

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,12 +5,14 @@ export interface AppConfig {
   appName: string;
   port: string;
   env: string;
+  swapiBaseUrl: string;
 }
 
 const loadConfig = (env: any): AppConfig => ({
   appName: env.APP_NAME ?? "boilerplate_api",
   port: env.PORT ?? "1337",
   env: env.STAGE,
+  swapiBaseUrl: env.SWAPI_BASE_URL,
 });
 
 const validateConfig = (config: AppConfig) => {
@@ -18,6 +20,7 @@ const validateConfig = (config: AppConfig) => {
     appName: Joi.string().required(),
     port: Joi.string().required(),
     env: Joi.string().required(),
+    swapiBaseUrl: Joi.string().required(),
   });
   const { error, value } = schema.validate(config);
 
---
+++ src/shared/swapi/swapi-path.enum.ts
@@ -0,0 +1,3 @@
+export enum SwapiPath {
+  PLANETS = "planets",
+}
